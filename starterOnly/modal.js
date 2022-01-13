9a52c6960d7e85760acbad6ae704fed039fe9441
jeremybosqui <bosdani97@gmail.com>
2022-01-13 15:52:17 -0400

    update modal js

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -27,3 +27,22 @@ closeModal.forEach((c) => c.addEventListener("click", modalClose));
   function modalClose(){
     modalbg.style.display = "none";
   }
+// implementation du formulaire
+
+           //liason des labels
+const firstName = document.getElementById("first");
+const lastName = document.getElementById("last");
+const eMail = document.getElementById("email");
+const birth = document.getElementById("birthdate");
+const numbTournament = document.getElementById("quantity");
+const location1 = document.getElementById("location1");
+const location2 = document.getElementById("location2");
+const location3 = document.getElementById("location3");
+const location4 = document.getElementById("location4");
+const location5 = document.getElementById("location5");
+const location6 = document.getElementById("location6");
+const checkbox1 = document.getElementById("checkbox1");
+const checkbox2 = document.getElementById("checkbox2");
+
+
+
