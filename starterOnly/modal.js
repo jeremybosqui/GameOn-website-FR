1fa3f7b51bbb91a92f44414bc8f1a99337abd6df
jeremybosqui <bosdani97@gmail.com>
2022-01-14 21:19:10 -0400

    update modal js

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -42,8 +42,44 @@ const loc4 = document.getElementById("location4");
 const loc5 = document.getElementById("location5");
 const loc6 = document.getElementById("location6");
 const checkBox1 = document.getElementById("checkbox1");
-const submitBtn = document.querySelector(".btn-submit")
+const submitBtn = document.querySelector(".btn-submit");
+// definition du format de la date
 
+// definition format des nombres
+const numbers = /^[0-9]+$/;
+// function validate modal form
+function validate () {
+  let firstNameCheck;
+  let lastNameCheck;
+  let eMailcheck;
+  let birthCheck;
+  let numTournamentCheck;
+  let radioButtonsCheck;
+  let conditionsCheck;
+
+  console.log(validate);
+
+ if (!firstName.value =="" || firstName.value == null || firstName.value.length < 2)
+  {}
+  else {}
+if (!lastName.value == "" || lastName.value == null || lastName.value.length < 2)
+  {}
+  else {}
+// email avec regex à definir if ()
+if(!birth.value.match(/^\d{4}\-(0?[1-9]|1[012]])\-(0?[1-9]|[12][0-9]|3[01])$/)) // defiinition de la birthdate avec un instruction en regex
+  {}
+  else {}
+if(!numbTournament.value.match(numbers)) 
+{}
+else {}
+if(!loc1.checked && !loc2.checked && !loc3.checked && !loc4.checked && !loc5.checked && !loc6.checked) 
+{}
+else {}
+if(!checkBox1.checked)
+{}
+else {}
+// validation si tous les elements checked sont valide 
+}
 
 
 
